5011360a6e5e8278552cc67faabcc939f0e2c5c8
Kusum Ratawa <kusum.ratawa@bacancytechnology.com>
2017-06-28 15:40:40 +0530

    Remove commented code.

--- client/src/libraries.js
+++ client/src/libraries.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 import Header from 'Common/Layouts/Header.js';
 import Footer from 'Common/Layouts/Footer.js';
-/*
-export const ReactApp = React;
-export const RouterApp = Router;
-export const RouteApp = Route;
-export const HeaderApp = Header;
-export const FooterApp = Footer;
-*/
+
 module.exports = {
   React,
   Router,
